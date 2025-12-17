a5d2fc3647f495a610c03f6b67caadd4b6fcc40d
jxnhoongz <vatanahan09@gmail.com>
2025-12-17 14:52:22 +0700

    add update and delete endpoints for categories and posts

--- lib/service/constant_uri.dart
+++ lib/service/constant_uri.dart
@@ -2,16 +2,21 @@ abstract class ConstantUri{
   static String baseUri = "https://learn-api.cambofreelance.com";
   static String loginPath = "$baseUri/api/oauth/token";
   static String registerPath = "$baseUri/api/oauth/register";
+  static String refreshTokenPath = "$baseUri/api/oauth/refresh";
 
   // Category endpoints
   static String categoryListPath = "$baseUri/api/app/category/list";
   static String categoryByIdPath = "$baseUri/api/app/category";
   static String categoryCreatePath = "$baseUri/api/app/category/create";
+  static String categoryUpdatePath = "$baseUri/api/app/category/update";
+  static String categoryDeletePath = "$baseUri/api/app/category/delete";
 
   // Post/Product endpoints
   static String postListPath = "$baseUri/api/app/post/list";
   static String postByIdPath = "$baseUri/api/app/post";
   static String postCreatePath = "$baseUri/api/app/post/create";
+  static String postUpdatePath = "$baseUri/api/app/post/update";
+  static String postDeletePath = "$baseUri/api/app/post/delete";
 
   // Image endpoints
   static String imageBasePath = "$baseUri/api/public/view/image";
